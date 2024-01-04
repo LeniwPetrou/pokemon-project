560b0d302901a5d7024eb3399b2bc0c1cce50230
LeniwPetrou <PETROU.ELENI1@nbg.gr>
2024-01-04 21:24:18 +0200

    changes in routing-config file.

--- src/app/shared/configuration/routing-config.ts
+++ src/app/shared/configuration/routing-config.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { PokemonComponent } from 'src/app/pokemon/component/pokemon.component';
 import { HomeComponent } from 'src/app/main/home/home.component';
 import { PokemonAbilitiesComponent } from 'src/app/pokemon-abilities/component/pokemon-abilities.component';
-import { PokemonSpeciesComponent } from 'src/app/pokemon copy/component/pokemon-species.component';
+import { PokemonSpeciesComponent } from 'src/app/pokemon-species/component/pokemon-species.component';
 
   export const routes: Routes = [
     { 
